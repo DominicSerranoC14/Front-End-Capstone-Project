3c87fc71e1cb228523a6bafc2033b9f87111dfb8
Dominic Serrano <dmsrojo@gmail.com>
2016-07-28 16:03:59 -0500

    Added dialog and redirected to login page

--- app/controllers/LoginCtrl.js
+++ app/controllers/LoginCtrl.js
@@ -1,24 +1,7 @@
 "use strict";
 
 //Controller for the HomeViewCtrl partial, will be ng-included in Index
-app.controller('LoginCtrl', function($scope) {
-
-  $scope.showConfirm = function(ev) {
-    // Appending dialog to document.body to cover sidenav in docs app
-    let confirm = $mdDialog.confirm()
-          .title('Would you like to delete your debt?')
-          .textContent('All of the banks have agreed to forgive you your debts.')
-          .ariaLabel('Lucky day')
-          .targetEvent(ev)
-          .ok('Please do it!')
-          .cancel('Sounds like a scam');
-    $mdDialog.show(confirm).then(function() {
-      $scope.status = 'You decided to get rid of your debt.';
-    }, function() {
-      $scope.status = 'You decided to keep your debt.';
-    });
-  };
-
+app.controller('LoginCtrl', function($scope, $mdDialog) {
 
   $scope.message = 'hello';
 
--- app/controllers/NavbarCtrl.js
+++ app/controllers/NavbarCtrl.js
@@ -1,13 +1,35 @@
 'use strict';
 
 //Controller for the Navbar partial, will be ng-included in Index
-app.controller('NavbarCtrl', function($scope) {
+app.controller('NavbarCtrl', function($scope, $location, $mdDialog) {
 
 
   //jQuery that activates drop down nav bar feature on page load
   $(document).ready(function() {
     $('#dropdown-button').dropdown();
   });
+
+
+  //////////////
+  // Angular for the dialog box confirming page leave on logout button press
+  // $mdDialog must be injected
+  /////////////
+  $scope.showConfirm = function(eventDiv) {
+    // Shows the contents of the dialog
+    let confirm = $mdDialog.confirm()
+          .title('Are you sure you want to leave this page?')
+          .targetEvent(eventDiv)
+          .ok('Leave Page')
+          .cancel('Cancel');
+    $mdDialog.show(confirm).then(function() {
+      //Insert log out function here
+      //Also redirects to login page
+      $mdDialog.confirm().
+        ok(console.log('works'),
+        $location.url("/login"));
+      });
+  };
+
 
   $scope.message = 'Hello';
 
