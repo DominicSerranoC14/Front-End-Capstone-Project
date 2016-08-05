b797e7da0f1ec10ec9236a07f8fce7ed93d69527
Dominic Serrano <dmsrojo@gmail.com>
2016-08-05 09:08:10 -0500

    Cleaned up auto comp spacing. Converted back to showing one fully built out item

--- app/controllers/ItemViewCtrl.js
+++ app/controllers/ItemViewCtrl.js
@@ -10,28 +10,28 @@ app.controller('ItemViewCtrl', function($scope, ItemSearchFactory) {
     desc : ""
   };
 
-  // $scope.orderObjects = function() {
-  //   console.log("Test $scope.segList", $scope.segList);
-  //   $scope.segList.sort(function( a, b ) {
-  //     if (a[0].order > b[0].order) {
-  //       return 1;
-  //     }
-  //     if (a[0].order < b[0].order) {
-  //       return -1;
-  //     }
-  //     // a must be equal to b
-  //     return 0;
-  //   });
-  // };
+  $scope.orderObjects = function() {
+    console.log("Test $scope.segList", $scope.segList);
+    $scope.segList.sort(function( a, b ) {
+      if (a[0].order > b[0].order) {
+        return 1;
+      }
+      if (a[0].order < b[0].order) {
+        return -1;
+      }
+      // a must be equal to b
+      return 0;
+    });
+  };
 
 
   //Get each segment object from the selected partnumber
-  let partNumber = ["BMP-121048-06096", "BMP-121048-09096", "BMP-121048-12096", "BMP-121048-060120", "BMP-121048-12120"];
+  let partNumber = "bmp-121048-06096";
+  // "BMP-121048-09096", "BMP-121048-12096", "BMP-121048-060120", "BMP-121048-12120"];
+  baseItemObject.itemNumber = partNumber;
+  let seg = partNumber.split('-');
 
   function loadAll() {
-  angular.forEach(partNumber, function(part) {
-  baseItemObject.itemNumber = part;
-  let seg = part.split('-');
 
     angular.forEach(seg, function(each) {
       console.log("Test each", each);
@@ -40,8 +40,8 @@ app.controller('ItemViewCtrl', function($scope, ItemSearchFactory) {
       .then(function(item) {
         $scope.segList.push(item);
         //sort segList after pushing each item
-        // $scope.orderObjects();
-        // baseItemObject.desc += item[0].desc;
+        $scope.orderObjects();
+        baseItemObject.desc += item[0].desc;
       });
     });//End of forEach
 
@@ -52,8 +52,7 @@ app.controller('ItemViewCtrl', function($scope, ItemSearchFactory) {
         baseItemObject.itemList.push(collection);
       });
     });//End forEach loop
-  });//End forEach partNumber
-  $scope.entireItem.push(baseItemObject);
+    $scope.entireItem.push(baseItemObject);
   }loadAll();
 
 
