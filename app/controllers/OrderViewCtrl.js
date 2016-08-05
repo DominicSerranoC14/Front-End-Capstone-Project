2f42c95822a4d6c7bd7399f7da3454d76d45a790
Dominic Serrano <dmsrojo@gmail.com>
2016-08-04 20:09:13 -0500

    Added grid and list view to order-view partial

--- app/controllers/OrderViewCtrl.js
+++ app/controllers/OrderViewCtrl.js
@@ -3,20 +3,40 @@
 //Controller for the order-view partial
 app.controller('OrderViewCtrl', function($scope, $location, $mdDialog, AuthFactory, OrderFactory) {
 
-
+  ////////////////////////////////////////////////////
+  // Load all orders and display to DOM
   OrderFactory.getCustomerOrder()
   .then(function(customerCollection) {
 
   $scope.customerOrderList = customerCollection;
-
-  
-    // Move this function to another view when the order is selected
-    // OrderFactory.getOrderItem(order.orderId)
-    // .then(function(itemCollection) {
-    //   console.log("Test itemCollection", itemCollection);
-    // });
-
   });
+  ////////////////////////////////////////////////////
+
+
+  ////////////////////////////////////////////////////
+  // Handle customer display change here, default is list view
+  //Sets the default view to list style
+  $scope.viewStyle = true;
+
+  //Changes view to list from grid
+  $scope.showList = function() {
+    $scope.viewStyle = true;
+  };
+
+  //Changes view to grid from list
+  $scope.showGrid = function() {
+    $scope.viewStyle = false;
+  };
+  ////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////
+
+  ////////////////////////////////////////////////////
+  //Search ability for customer view
+  //works in grid and list views
+  //Links the orders to the search bar which is ng-modeled to searchText
+  $scope.searchText = $scope.customerOrderList;
+  ////////////////////////////////////////////////////
+
 
 
 
